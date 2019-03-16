a908875e03b7ecd15b6caa90ebce77d5cca5d82f
ghigbie <gehigbie@gmail.com>
2019-03-16 08:54:23 -0600

    Add error handler

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ title = "Flask with Databases"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title=title)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('notfound.html', title=title, e=e)
 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)
