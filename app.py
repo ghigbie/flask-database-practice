d72e7673d6c82334c235296dd0635d36448fb765
ghigbie <gehigbie@gmail.com>
2019-03-16 09:16:21 -0600

    Add about route

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 
 title = "Flask with Databases"
@@ -8,6 +13,10 @@ title = "Flask with Databases"
 def index():
     return render_template('index.html', title=title)
 
+@app.route('about')
+def about():
+    return render_template('about.html', title=title)
+    
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('notfound.html', title=title, e=e)
