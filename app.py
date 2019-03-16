459f84af317f468827f74bd5762a4a3582f785c9
ghigbie <gehigbie@gmail.com>
2019-03-16 09:37:28 -0600

    Edit basic model

--- app.py
+++ app.py
@@ -3,11 +3,31 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
 
-title = "Flask with Databases"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+##############################################################################
+
+class Puppy(db.Model):
+    __tablename__ = 'puppies'  # This overides the default table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    fur = db.Column(db.Text)
+    sex = db.Column(db.Text)
+
+    def __init__(self,name,age): #initiates the object
+        self.name = name
+        self.age = age
+
+    def __repr__(self): #Provides a string representation of the object
+        return f"Puppy with a name of {self.name} is {self.age} year(s) old. His/Her fur color is: {self.color}."
+
+title = "Flask with Databases"
 
 @app.route('/')
 def index():
